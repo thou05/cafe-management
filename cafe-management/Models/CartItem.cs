d836cfa437e3469652520452cbac217cc84280a3
nhylee <nhyen875@gmail.com>
2025-11-15 00:39:17 +0700

    commit sanpham, home, cart

---
+++ cafe-management/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace cafe_management.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Img { get; set; } = null!;
+        public decimal? Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal? TotalAmount => Quantity * Price;
+    }
+}
