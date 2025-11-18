14d5f29082a38db665c84569448c0fd6c44bf274
nhylee <nhyen875@gmail.com>
2025-11-18 22:27:05 +0700

    contact, news, about

---
+++ cafe-management/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using cafe_management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cafe_management.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        private readonly CafeDBContext _context;
+
+        public ContactController(CafeDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // Model changed from TbPhanHoi -> TbFeedback
+        // Variable changed from phanHoi -> feedback
+        public IActionResult Index(TbFeedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // DbSet changed from TbPhanHois -> TbFeedbacks
+                    _context.TbFeedbacks.Add(feedback);
+                    _context.SaveChanges();
+                    TempData["Status"] = "success";
+                    // Message translated
+                    TempData["Message"] = "Feedback submitted successfully.";
+                    return RedirectToAction("index");
+                }
+                catch
+                {
+                    TempData["Status"] = "error";
+                    TempData["Message"] = "Failed to submit feedback.";
+                }
+            }
+
+            // Return the object in case of validation failure
+            return View(feedback);
+        }
+    }
+}
---
+++ cafe-management/Controllers/NewsController.cs
@@ -0,0 +1,34 @@
+﻿using cafe_management.Models;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+using Microsoft.EntityFrameworkCore;
+
+namespace cafe_management.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly CafeDBContext _context;
+
+        public NewsController(CafeDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 9;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listItem = _context.TbNews.AsNoTracking().OrderByDescending(x => x.PostedDate).ToList();
+            PagedList<TbNews> pagedListItem = new PagedList<TbNews>(listItem, pageNumber, pageSize);
+
+            return View(pagedListItem);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var newPost = _context.TbNews.SingleOrDefault(x => x.Id == id);
+
+            return View(newPost);
+        }
+    }
+}
