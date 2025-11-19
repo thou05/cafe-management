91b814783e3c82e51edda544cdd4c63d8375167a
Cao Xuan <xuancao701@gmail.com>
2025-11-19 11:55:00 +0700

    acc bill client feedback

---
+++ cafe-management/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using cafe_management.Models;
+using cafe_management.Models.Authentication;
+using X.PagedList;
+
+namespace cafe_management.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Accounts")]
+    public class AccountsController : Controller
+    {
+        private readonly CafeDBContext _context;
+
+        public AccountsController(CafeDBContext context)
+        {
+            _context = context;
+        }
+
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Convert the password string to a byte array
+                byte[] passwor_contextytes = Encoding.UTF8.GetBytes(password);
+
+                // Compute the SHA-256 hash of the password bytes
+                byte[] hashBytes = sha256.ComputeHash(passwor_contextytes);
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        [Route("")]
+        [Route("Index")]
+        [Authentication]
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 30;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listItem = _context.TbAccounts.AsNoTracking().OrderBy(x => x.UserName).ToList();
+            PagedList<TbAccount> pagedListItem = new PagedList<TbAccount>(listItem, pageNumber, pageSize);
+
+            return View(pagedListItem);
+        }
+
+        [Route("Create")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("Create")]
+        [Authentication]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TbAccount quanTriVien)
+        {
+            _context.TbAccounts.Add(quanTriVien);
+            _context.SaveChanges();
+            TempData["Message"] = "Thêm thành công";
+
+            return RedirectToAction("Index", "Accounts");
+        }
+
+        [Route("Edit")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Edit(int id, string name)
+        {
+            var quanTriVien = _context.TbAccounts.Find(id);
+            ViewBag.id = id;
+
+            return View(quanTriVien);
+        }
+
+        [Route("Edit")]
+        [Authentication]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TbAccount quanTriVien)
+        {
+            string hashPass = HashPassword(quanTriVien.Password);
+
+            quanTriVien.Password = hashPass;
+
+            _context.Entry(quanTriVien).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            TempData["Message"] = "Sửa thành công";
+
+            return RedirectToAction("Index", "Accounts");
+        }
+
+        [Route("Delete")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            TempData["Message"] = "";
+
+            _context.Remove(_context.TbAccounts.Find(id));
+            _context.SaveChanges();
+
+            TempData["Message"] = "Xoá thành công";
+
+            return RedirectToAction("Index", "Accounts");
+        }
+    }
+}
---
+++ cafe-management/Areas/Admin/Controllers/BillController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using cafe_management.Models;
+using cafe_management.Models.Authentication;
+using X.PagedList;
+
+namespace cafe_management.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Bill")]
+    public class BillController : Controller
+    {
+        private readonly CafeDBContext _context;
+
+        public BillController(CafeDBContext context)
+        {
+            _context = context;
+        }
+
+        [Route("")]
+        [Route("Index")]
+        [Authentication]
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 30;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listItem = _context.TbBills.AsNoTracking().OrderByDescending(x => x.OrderDate).ToList();
+            PagedList<TbBill> pagedListItem = new PagedList<TbBill>(listItem, pageNumber, pageSize);
+
+            return View(pagedListItem);
+        }
+
+        [Route("Search")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Search(int? page, string search)
+        {
+            int pageSize = 30;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+
+            ViewBag.search = search;
+
+            var listItem = _context.TbBills.AsNoTracking().Where(x => x.OrderDate.ToString().Contains(search)).OrderBy(x => x.Id).ToList();
+            PagedList<TbBill> pagedListItem = new PagedList<TbBill>(listItem, pageNumber, pageSize);
+
+            return View(pagedListItem);
+        }
+
+        [Route("Details")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Details(int? page, string id, string name)
+        {
+            int pageSize = 30;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listItem = _context.TbBillDetails.AsNoTracking().Where(x => x.BillId == Guid.Parse(id)).OrderBy(x => x.BillId).ToList();
+            PagedList<TbBillDetail> pagedListItem = new PagedList<TbBillDetail>(listItem, pageNumber, pageSize);
+
+            ViewBag.name = name;
+
+            return View(pagedListItem);
+        }
+    }
+}
