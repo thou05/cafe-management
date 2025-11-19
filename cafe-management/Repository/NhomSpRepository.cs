b344e040b3cdedd4add9f0ec9c5da4bc91269431
nhylee <nhyen875@gmail.com>
2025-11-19 17:52:35 +0700

    Repository, ViewComponents

---
+++ cafe-management/Repository/NhomSpRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cafe_management.Models;
+
+namespace  cafe_management.Repository
+{
+    public class NhomSpRepository : INhomSpRepository
+    {
+        private readonly CafeDBContext _context;
+
+        public NhomSpRepository(CafeDBContext context)
+        {
+            _context = context;
+        }
+
+        public TbCategory Add(TbCategory nhomSanPham)
+        {
+            _context.TbCategories.Add(nhomSanPham);
+            _context.SaveChanges();
+            return nhomSanPham;
+        }
+
+        public TbCategory Delete(string maNhomSp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TbCategory> GetAllNhomSp()
+        {
+            return _context.TbCategories;
+        }
+
+        public TbCategory GetAllNhomSp(string maNhomSp)
+        {
+            return _context.TbCategories.Find(maNhomSp);
+        }
+
+        public TbCategory Update(TbCategory nhomSp)
+        {
+            _context.Update(nhomSp);
+            _context.SaveChanges();
+            return nhomSp;
+        }
+    }
+}
---
+++ cafe-management/ViewComponents/NhomSpMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using cafe_management.Repository;
+
+namespace cafe_management.ViewComponents
+{
+    public class NhomSpMenuViewComponent : ViewComponent
+    {
+        private readonly INhomSpRepository _nhomSp;
+        public NhomSpMenuViewComponent(INhomSpRepository nhomSpRepository)
+        {
+            _nhomSp = nhomSpRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var nhomSp = _nhomSp.GetAllNhomSp().OrderBy(x => x.Name);
+            return View(nhomSp);
+        }
+    }
+}
