0ca71a631cb1aee39123d12da1257a20e8512890
thou05 <thaao.lt@gmail.com>
2025-11-21 23:06:20 +0700

    fix nav admin color

--- cafe-management/obj/Debug/net8.0/cafe-management.AssemblyInfo.cs
+++ cafe-management/obj/Debug/net8.0/cafe-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cafe-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d61a41e62a7de2bcbe653fbb22bb10691a3a9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf27d26cbd9d168fb51e57ef11dd273f48526c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cafe-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cafe-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
